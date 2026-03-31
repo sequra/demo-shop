d2caea4a7cf87f7ea5260161d20b6dfc486f4ad3
Michel Escalante Álvarez <michel.escalante@sequra.es>
2026-03-31 16:39:39 +0200

    Update product prices and quantities in products.js

--- src/frontend/src/data/products.js
+++ src/frontend/src/data/products.js
@@ -2,7 +2,7 @@ export const products = [
   {
     id: 1,
     nameKey: 'product.headphones.name',
-    price: 22.00,
+    price: 24.00,
     image: '/dist/images/products/headphones.jpg',
     descKey: 'product.headphones.desc',
     quantity: 2
@@ -10,57 +10,57 @@ export const products = [
   {
     id: 2,
     nameKey: 'product.smartwatch.name',
-    price: 12.00,
+    price: 200.00,
     image: '/dist/images/products/smartwatch.jpg',
     descKey: 'product.smartwatch.desc',
-    quantity: 3
+    quantity: 1
   },
   {
     id: 3,
     nameKey: 'product.laptop.name',
-    price: 12.00,
+    price: 400.00,
     image: '/dist/images/products/laptop.jpg',
     descKey: 'product.laptop.desc',
-    quantity: 2
+    quantity: 1
   },
   {
     id: 4,
     nameKey: 'product.keyboard.name',
-    price: 13.00,
+    price: 49.00,
     image: '/dist/images/products/keyboard.jpg',
     descKey: 'product.keyboard.desc',
     quantity: 1
   },
-  {
-    id: 5,
-    nameKey: 'product.speaker.name',
-    price: 32.00,
-    image: '/dist/images/products/speaker.jpg',
-    descKey: 'product.speaker.desc',
-    quantity: 1
-  },
-  {
-    id: 6,
-    nameKey: 'product.iphone.name',
-    price: 12.15,
-    image: '/dist/images/products/iphone.jpg',
-    descKey: 'product.iphone.desc',
-    quantity: 3
-  },
-  {
-    id: 7,
-    nameKey: 'product.ipad.name',
-    price: 3.00,
-    image: '/dist/images/products/ipad.jpg',
-    descKey: 'product.ipad.desc',
-    quantity: 7
-  },
-  {
-    id: 8,
-    nameKey: 'product.earbuds.name',
-    price: 2.00,
-    image: '/dist/images/products/earbuds.jpg',
-    descKey: 'product.earbuds.desc',
-    quantity: 2
-  }
+  // {
+  //   id: 5,
+  //   nameKey: 'product.speaker.name',
+  //   price: 32.00,
+  //   image: '/dist/images/products/speaker.jpg',
+  //   descKey: 'product.speaker.desc',
+  //   quantity: 1
+  // },
+  // {
+  //   id: 6,
+  //   nameKey: 'product.iphone.name',
+  //   price: 12.15,
+  //   image: '/dist/images/products/iphone.jpg',
+  //   descKey: 'product.iphone.desc',
+  //   quantity: 3
+  // },
+  // {
+  //   id: 7,
+  //   nameKey: 'product.ipad.name',
+  //   price: 3.00,
+  //   image: '/dist/images/products/ipad.jpg',
+  //   descKey: 'product.ipad.desc',
+  //   quantity: 7
+  // },
+  // {
+  //   id: 8,
+  //   nameKey: 'product.earbuds.name',
+  //   price: 2.00,
+  //   image: '/dist/images/products/earbuds.jpg',
+  //   descKey: 'product.earbuds.desc',
+  //   quantity: 2
+  // }
 ];
